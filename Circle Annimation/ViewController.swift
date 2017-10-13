bd60a2c62bb9eda82226cd3911ccae5dc9975479
Warren Hansen <whansen1@mac.com>
2017-10-12 23:31:54 -0700

    initial commit

--- Circle Annimation/ViewController.swift
+++ Circle Annimation/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var someView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let replicatorLayer = CAReplicatorLayer()
+        replicatorLayer.frame = someView.bounds
+        replicatorLayer.instanceCount = 30
+        replicatorLayer.instanceDelay = CFTimeInterval(1 / 30.0)
+        replicatorLayer.preservesDepth = false
+        replicatorLayer.instanceColor = UIColor.white.cgColor
+        
+        replicatorLayer.instanceRedOffset = -0.5
+        replicatorLayer.instanceGreenOffset = -0.5
+        replicatorLayer.instanceBlueOffset = -0.5
+        replicatorLayer.instanceAlphaOffset = 0.0
+        
+        let angle = Float(Double.pi * 2.0) / 30
+        replicatorLayer.instanceTransform = CATransform3DMakeRotation(CGFloat(angle), 0.0, 0.0, 1.0)
+        someView.layer.addSublayer(replicatorLayer)
+        
+        let instanceLayer = CALayer()
+        let layerWidth: CGFloat = 10.0
+        let midX = someView.bounds.midX - layerWidth / 2.0
+        instanceLayer.frame = CGRect(x: midX, y: 0.0, width: layerWidth, height: layerWidth * 3.0)
+        instanceLayer.backgroundColor = UIColor.white.cgColor
+        replicatorLayer.addSublayer(instanceLayer)
+        
+        let fadeAnimation = CABasicAnimation(keyPath: "opacity")
+        fadeAnimation.fromValue = 1.0
+        fadeAnimation.toValue = 0.0
+        fadeAnimation.duration = 1
+        fadeAnimation.repeatCount = Float.greatestFiniteMagnitude
+        
+        instanceLayer.opacity = 0.0
+        instanceLayer.add(fadeAnimation, forKey: "FadeAnimation")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func animateAction(_ sender: Any) {
+        
     }
-
-
+    
 }
 
